c78b186b756f1f5438229baa44ab0afccdfc2e41
superherointj <5861043+superherointj@users.noreply.github.com>
2023-05-08 10:03:48 -0300

    init

--- pkgs/python/overlay.nix
+++ pkgs/python/overlay.nix
@@ -5,13 +5,10 @@ final: prev: {
       {
         abstract-singleton = callPyPkg ./abstract-singleton { };
         auto-gpt-plugin-template = callPyPkg ./auto-gpt-plugin-template { };
+        openapi-python-client = callPyPkg ./openapi-python-client { };
         sourcery = callPyPkg ./sourcery { };
+        wandb = python-prev.wandb.overrideAttrs(oa: { disabledTests = [ "test_artifacts_cache_cleanup" ]; });
         webdriver-manager = callPyPkg ./webdriver-manager { };
-        wandb = python-prev.wandb.overrideAttrs(oa: {
-          disabledTests = [
-            "test_artifacts_cache_cleanup"
-          ];
-        });
       }
     )
   ];
