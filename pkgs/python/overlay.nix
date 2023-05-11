13176986a4e62d7044132149ed8f42460b70b81e
superherointj <5861043+superherointj@users.noreply.github.com>
2023-05-11 09:07:09 -0300

    top-level.python: spaced lines

--- pkgs/python/overlay.nix
+++ pkgs/python/overlay.nix
@@ -4,22 +4,41 @@ final: prev: {
       let callPyPkg = python-final.callPackage; in
       {
         abstract-singleton = callPyPkg ./abstract-singleton { };
+
         auto-gpt-plugin-template = callPyPkg ./auto-gpt-plugin-template { };
+
         composer = callPyPkg ./composer { };
+
         coolname = callPyPkg ./coolname { };
+
         envier = callPyPkg ./envier { };
+
         flake8-quotes = callPyPkg ./flake8-quotes { };
+
         loralib = callPyPkg ./loralib { };
+
         openapi-python-client = callPyPkg ./openapi-python-client { };
+
         opentelemetry-api = callPyPkg ./opentelemetry-api { };
+
         pyre-check = callPyPkg ./pyre-check { };
+
         pyre-extensions = callPyPkg ./pyre-extensions { };
+
         pytorch-ranger = callPyPkg ./pytorch-ranger { };
+
         sourcery = callPyPkg ./sourcery { };
+
         testslide = callPyPkg ./testslide { };
+
         torch-optimizer = callPyPkg ./torch-optimizer { };
-        wandb = python-prev.wandb.overrideAttrs(oa: { disabledTests = [ "test_artifacts_cache_cleanup" ]; });
+
+        wandb = python-prev.wandb.overrideAttrs(oa: {
+          disabledTests = [ "test_artifacts_cache_cleanup" ];
+        });
+
         webdriver-manager = callPyPkg ./webdriver-manager { };
+
         xformers = callPyPkg ./xformers { };
       }
     )
