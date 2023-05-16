4bf978484da08d6d6f9bf95891ec1eb4027ec1e2
superherointj <5861043+superherointj@users.noreply.github.com>
2023-05-15 21:08:46 -0300

    fluxcd: 2.0.0-rc.2 -> 2.0.0-rc.3

--- pkgs/go/overlay.nix
+++ pkgs/go/overlay.nix
@@ -5,6 +5,8 @@ let
   callPackage = final.callPackage;
 in
 rec {
+  fluxcd = callPackage ./fluxcd { };
+
   k3s_1_26 = callPackage ./k3s/1_26 { };
   k3s_1_27 = callPackage ./k3s/1_27 { };
   k3s = k3s_1_27;
