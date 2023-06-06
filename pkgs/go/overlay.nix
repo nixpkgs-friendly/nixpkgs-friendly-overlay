eb55682619f2488e9b788eb409ad56bbe24fa45f
superherointj <5861043+superherointj@users.noreply.github.com>
2023-06-06 18:13:57 -0300

    clean-up

--- pkgs/go/overlay.nix
+++ pkgs/go/overlay.nix
@@ -1,19 +1,15 @@
 final: prev:
 
 let
-  fetchpatch2 = prev.fetchpatch2;
   callPackage = final.callPackage;
 in
 rec {
-  # etcd_3_5 = callPackage ./etcd/3_5 { };
-  # etcd = etcd_3_5;
-
   fluxcd = callPackage ./fluxcd { };
 
   k3s_1_26 = callPackage ./k3s/1_26 { };
   k3s_1_27 = callPackage ./k3s/1_27 { };
   k3s = k3s_1_27;
-
-  # Fix-Me: Fix workaround for K3s update script
+  # Fix-Me: Workaround for K3s update script
   k3s_noVendorHash = k3s.go-modules.overrideAttrs (_: { vendorSha256 = ""; });
+
 }
