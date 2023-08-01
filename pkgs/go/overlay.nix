a0bee87c748a9e6645b11d385e49dbf34c133349
superherointj <5861043+superherointj@users.noreply.github.com>
2023-08-01 13:05:35 -0300

    fluxcd: disable

--- pkgs/go/overlay.nix
+++ pkgs/go/overlay.nix
@@ -4,7 +4,7 @@ let
   callPackage = final.callPackage;
 in
 rec {
-  fluxcd = callPackage ./fluxcd { };
+  # fluxcd = callPackage ./fluxcd { };
 
   # k3s_1_26 = callPackage ./k3s/1_26 { };
   # k3s_1_27 = callPackage ./k3s/1_27 { };
