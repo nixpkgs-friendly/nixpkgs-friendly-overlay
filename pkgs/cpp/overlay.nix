99f33c04143dac956dc1ff660af281e8a2a4d0a9
superherointj <5861043+superherointj@users.noreply.github.com>
2023-11-10 14:10:53 -0300

    godot_4_2_beta5 fix commit hash

--- pkgs/cpp/overlay.nix
+++ pkgs/cpp/overlay.nix
@@ -8,17 +8,23 @@ rec {
   godot_4-debug = godot_4.override({ withDebug = true; });
   godot_4-export-templates = callPackage ./godot/4/export-templates.nix { };
 
-  godot_4_2_beta5 = godot_4.overrideAttrs(oa: {
-    version = "4.2-beta5";
-    src = final.fetchFromGitHub {
-      owner = "godotengine";
-      repo = "godot";
-      # Hash from: https://github.com/godotengine/godot-builds/releases/tag/4.2-beta5
-      rev = "4c96e9676b66d0cc9a25022b019b78f4c20ddc60";
-      hash = "sha256-3dwkZINT9DihUIScVLXDFONMEy7mAaoikW0GDNiskgM=";
-    };
-    GODOT_VERSION_STATUS="beta5";
-  });
+  godot_4_2_beta5 =
+    let commitHash = "4c96e9676b66d0cc9a25022b019b78f4c20ddc60"; in
+    godot_4.overrideAttrs(oa: {
+      version = "4.2-beta5";
+      src = final.fetchFromGitHub {
+        owner = "godotengine";
+        repo = "godot";
+        # Hash from: https://github.com/godotengine/godot-builds/releases/tag/4.2-beta5
+        rev = commitHash;
+        hash = "sha256-3dwkZINT9DihUIScVLXDFONMEy7mAaoikW0GDNiskgM=";
+      };
+      GODOT_VERSION_STATUS="beta5";
+      preConfigure = ''
+        mkdir -p .git
+        echo ${commitHash} > .git/HEAD
+      '';
+    });
   godot_4_2_beta5-debug = godot_4_2_beta5.override({ withDebug = true; });
   godot_4_2_beta5-export-templates = final.fetchzip {
     url = "https://github.com/godotengine/godot-builds/releases/download/4.2-beta5/Godot_v4.2-beta5_export_templates.tpz";
