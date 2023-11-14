53de1b717db11dd58cd53e5b89c6acf6b31984cb
superherointj <5861043+superherointj@users.noreply.github.com>
2023-11-14 09:28:50 -0300

    godot: improve generic

--- pkgs/cpp/overlay.nix
+++ pkgs/cpp/overlay.nix
@@ -41,34 +41,37 @@ let
         godot_4 = callPackage ./godot/4 { };
         godot_4-debug = self.godot_4.override({ withDebug = true; });
         godot_4-export-templates = callPackage ./godot/4/export-templates.nix { };
-      } // (generic({
+      } //
+      (prev.lib.foldr (item: acc: acc // generic(item)) {} [
+        {
           majorMinorVersion = "4.2";
           versionStatus = "beta6";
           commit = "64150060f89677eaf11229813ae6c5cf8a873802";
           hash = "sha256-Q7OmQIbvHx+Re4vlov4ylmWzhWJL4GWzr2AYkSk0fiQ=";
           exportTemplatesHash = "sha256-3Qi6lIHIvJ8DMDL8AdmOa/xasphnCYGoGIdNjXHZH2o=";
-        }))
-        // (generic({
+        }
+        {
           majorMinorVersion = "4.2";
           versionStatus = "beta5";
           commit = "4c96e9676b66d0cc9a25022b019b78f4c20ddc60";
           hash = "sha256-3dwkZINT9DihUIScVLXDFONMEy7mAaoikW0GDNiskgM=";
           exportTemplatesHash = "sha256-JoZ5xzVaaVScbthE79KRB6EDtrTKLHbKSUbh6ZrNhlk=";
-        }))
-        // (generic({
+        }
+        {
           majorMinorVersion = "4.1.3";
           versionStatus = "stable";
           commit = "fc79201851a16215f9554884aa242ed957801b10";
           hash = "sha256-z5JRPhdEO20AodS12MApgur0BMHGToUjo2r2eI77nNc=";
           exportTemplatesHash = "sha256-P3CkOyR+IT/KDuV6WpL8yOd97jOTYuRGGhLo4ObsDvM=";
-        }))
-        # // (generic({
+        }
+        # {
         #   commit = "bdd9034ad05e1824ff5d9c750acd87caeafe6dca";
         #   majorMinorVersion = "4.2";
         #   versionStatus = "master";
         #   hash = "sha256-zXjUVs0LbIeKlj+D+C0pPV8P6iN0+zrAF9Gen3LHzV0=";
         #   # exportTemplatesHash = "";
-        # }))
+        # }
+      ])
     )
   ) self; in self
 
