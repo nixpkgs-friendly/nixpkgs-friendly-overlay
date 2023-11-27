6c7051c84dd5da93f6b8350c155392512c98099d
superherointj <5861043+superherointj@users.noreply.github.com>
2023-11-27 13:19:28 -0300

    k3s: remove

--- pkgs/go/overlay.nix
+++ pkgs/go/overlay.nix
@@ -3,11 +3,8 @@ final: prev:
 let
   callPackage = final.callPackage;
 in
-rec {
-  # fluxcd = callPackage ./fluxcd { };
 
-  # k3s_1_26 = callPackage ./k3s/1_26 { };
-  # k3s_1_27 = callPackage ./k3s/1_27 { };
-  # k3s = k3s_1_27;
+rec {
+
 
 }
