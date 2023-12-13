36a28fbee10748185f36d4d7eb27b0fca2b2cf91
superherointj <5861043+superherointj@users.noreply.github.com>
2023-12-13 12:49:09 -0300

    disable yandex-browser

--- pkgs/misc/overlay.nix
+++ pkgs/misc/overlay.nix
@@ -5,12 +5,12 @@ let
 in
 rec {
 
-  yandex-browser-stable = callPackage ./yandex-browser { };
-  yandex-browser-beta = yandex-browser.override({
-    channel = "beta";
-    version = "23.5.1.754-1";
-    hash = "sha256-ngtwrq8vDEt39Zd5jpBadouN1V8ly03la69M0AUyhGM=";
-  });
-  yandex-browser = yandex-browser-stable;
+  #yandex-browser-stable = callPackage ./yandex-browser { };
+  #yandex-browser-beta = yandex-browser.override({
+  #  channel = "beta";
+  #  version = "23.5.1.754-1";
+  #  hash = "sha256-ngtwrq8vDEt39Zd5jpBadouN1V8ly03la69M0AUyhGM=";
+  #});
+  #yandex-browser = yandex-browser-stable;
 
 }
