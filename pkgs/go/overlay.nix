3934674267863dfdda6a0605d6bcaa690fcc26e0
superherointj <5861043+superherointj@users.noreply.github.com>
2023-12-16 10:36:46 -0300

    vcluster: 0.17.0 -> 0.18.1

--- pkgs/go/overlay.nix
+++ pkgs/go/overlay.nix
@@ -12,4 +12,24 @@ rec {
   k3s = k3s_1_28;
 
   kubevela = callPackage ./kubevela { };
+
+  vcluster = prev.vcluster.overrideAttrs(oa: rec {
+    version = "0.18.1";
+    src = prev.fetchFromGitHub {
+      owner = "loft-sh";
+      repo = "vcluster";
+      rev = "v0.18.1"; # ${version}
+      hash = "sha256-TJjMB7x8MOlr3GexsnOZBFPJovVkf4ByRn1aGprvZFQ=";
+    };
+    ldflags = [
+      "-s" "-w"
+      "-X main.version=${version}"
+      "-X main.goVersion=${prev.lib.getVersion final.go}"
+    ];
+    passthru.tests.version = prev.testers.testVersion {
+      package = final.vcluster;
+      command = "vcluster --version";
+    };
+  });
+
 }
