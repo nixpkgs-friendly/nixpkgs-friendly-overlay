707092a37523fff1a2f012b64cf5182d81313fa7
superherointj <5861043+superherointj@users.noreply.github.com>
2023-12-17 12:12:38 -0300

    migrate to pickLatest abstraction; disable k3s

--- pkgs/go/overlay.nix
+++ pkgs/go/overlay.nix
@@ -3,40 +3,48 @@ final: prev:
 let
   callPackage = final.callPackage;
   buildGo120Module = final.buildGo120Module;
+  pickLatest = newp: oldp:
+    if newp == null
+      then if oldp != null
+        then oldp
+        else throw "pickLatest: both packages can't be null at the same time."
+      else if oldp == null
+        then newp
+        else if (builtins.compareVersions newp.version oldp.version) > 0
+          then newp
+          else
+            (if (newp.version == oldp.version)
+              then builtins.trace "deprecated: override of ${newp.pname} (${newp.version}) has reached version parity. Consider removing it."
+              else builtins.trace "deprecated: override of ${newp.pname} (${newp.version}) is outdated, current version is ${oldp.version}. Consider removing it."
+            ) oldp;
 in
 
 rec {
-  fluxcd = let newFluxCD = callPackage ./fluxcd { }; in
-    if (builtins.compareVersions newFluxCD.version prev.fluxcd.version) > 0
-      then newFluxCD
-      else
-        builtins.trace "deprecated: override of fluxcd (${newFluxCD.version}) is outdated. Current upstream version is ${prev.fluxcd.version}." prev.fluxcd;
+  fluxcd = pickLatest (callPackage ./fluxcd { }) prev.fluxcd;
 
-  inherit (callPackage ./k3s { buildGoModule = buildGo120Module; }) k3s_1_28;
-  k3s = k3s_1_28;
+  # k3s_1_28 = pickLatest (callPackage ./k3s { buildGoModule = buildGo120Module; }).k3s_1_28 (prev.k3s_1_28 or null);
+  # k3s = pickLatest k3s_1_28 prev.k3s;
 
-  kubevela = callPackage ./kubevela { };
+  kubevela = pickLatest (callPackage ./kubevela { }) (prev.kubevela or null);
 
-  vcluster = let newVCluster = prev.vcluster.overrideAttrs(oa: rec {
-    version = "0.18.1";
-    src = prev.fetchFromGitHub {
-      owner = "loft-sh";
-      repo = "vcluster";
-      rev = "v0.18.1"; # ${version}
-      hash = "sha256-TJjMB7x8MOlr3GexsnOZBFPJovVkf4ByRn1aGprvZFQ=";
-    };
-    ldflags = [
-      "-s" "-w"
-      "-X main.version=${version}"
-      "-X main.goVersion=${prev.lib.getVersion final.go}"
-    ];
-    passthru.tests.version = prev.testers.testVersion {
-      package = final.vcluster;
-      command = "vcluster --version";
-    };
-  }); in
-    if (builtins.compareVersions newVCluster.version prev.vcluster.version) > 0
-      then newVCluster
-      else builtins.trace "deprecated: override of vcluster (${newVCluster.version}) is outdated. Current vcluster version is ${prev.vcluster}." prev.vcluster;
+  vcluster = let newVCluster = prev.vcluster.overrideAttrs(oa:
+    rec {
+      version = "0.18.1";
+      src = prev.fetchFromGitHub {
+        owner = "loft-sh";
+        repo = "vcluster";
+        rev = "v0.18.1"; # ${version}
+        hash = "sha256-TJjMB7x8MOlr3GexsnOZBFPJovVkf4ByRn1aGprvZFQ=";
+      };
+      ldflags = [
+        "-s" "-w"
+        "-X main.version=${version}"
+        "-X main.goVersion=${prev.lib.getVersion final.go}"
+      ];
+      passthru.tests.version = prev.testers.testVersion {
+        package = final.vcluster;
+        command = "vcluster --version";
+      };
+    }); in pickLatest newVCluster prev.vcluster;
 
 }
