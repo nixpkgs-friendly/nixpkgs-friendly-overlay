3af7d6ff784f1960d1aa773d7e6e7d817885206f
superherointj <5861043+superherointj@users.noreply.github.com>
2023-12-20 13:57:01 -0300

    vcluster: move to discrete file

--- pkgs/go/overlay.nix
+++ pkgs/go/overlay.nix
@@ -14,24 +14,5 @@ rec {
 
   kubevela = pickLatest (callPackage ./kubevela { }) (prev.kubevela or null);
 
-  vcluster = let newVCluster = prev.vcluster.overrideAttrs(oa:
-    rec {
-      version = "0.18.1";
-      src = prev.fetchFromGitHub {
-        owner = "loft-sh";
-        repo = "vcluster";
-        rev = "v${version}";
-        hash = "sha256-TJjMB7x8MOlr3GexsnOZBFPJovVkf4ByRn1aGprvZFQ=";
-      };
-      ldflags = [
-        "-s" "-w"
-        "-X main.version=${version}"
-        "-X main.goVersion=${prev.lib.getVersion final.go}"
-      ];
-      passthru.tests.version = prev.testers.testVersion {
-        package = final.vcluster;
-        command = "vcluster --version";
-      };
-    }); in pickLatest newVCluster prev.vcluster;
-
+  vcluster = pickLatest ((import ./vcluster) final prev) prev.vcluster;
 }
