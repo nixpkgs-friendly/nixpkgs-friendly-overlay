4cc5e0a44da3f06411ae908e198d96c100737bb2
superherointj <5861043+superherointj@users.noreply.github.com>
2024-01-14 19:38:08 -0300

    kubevela: remove

--- pkgs/go/overlay.nix
+++ pkgs/go/overlay.nix
@@ -18,7 +18,5 @@ rec {
   k3s_1_29 = pickLatest (callPackage ./k3s { buildGoModule = buildGo121Module; }).k3s_1_29 (prev.k3s_1_29 or null);
   k3s = pickLatest k3s_1_29 prev.k3s;
 
-  kubevela = pickLatest (callPackage ./kubevela { }) (prev.kubevela or null);
-
   # vcluster = pickLatest ((import ./vcluster) final prev) prev.vcluster;
 }
