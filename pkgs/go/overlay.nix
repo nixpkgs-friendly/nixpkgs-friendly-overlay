3f42b1373f71d67edd7b14648d606f2175d0221d
superherointj <5861043+superherointj@users.noreply.github.com>
2024-02-08 21:23:38 -0300

    fluxcd: disable in favor of upstream

--- pkgs/go/overlay.nix
+++ pkgs/go/overlay.nix
@@ -12,7 +12,7 @@ rec {
 
   etcd_3_5 = pickLatest (callPackage ./etcd { }) prev.etcd_3_5;
 
-  fluxcd = pickLatest (callPackage ./fluxcd { }) prev.fluxcd;
+  # fluxcd = pickLatest (callPackage ./fluxcd { }) prev.fluxcd;
 
   # k3s_1_28 = pickLatest (callPackage ./k3s { buildGoModule = buildGo120Module; }).k3s_1_28 (prev.k3s_1_28 or null);
   # k3s_1_29 = pickLatest (callPackage ./k3s { buildGoModule = buildGo121Module; }).k3s_1_29 (prev.k3s_1_29 or null);
