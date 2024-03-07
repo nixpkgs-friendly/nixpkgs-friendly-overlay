309562b06079eeac926a386a9ee12614b2e08428
superherointj <5861043+superherointj@users.noreply.github.com>
2024-03-07 18:37:29 -0300

    python3Packages.envier: 0.4.0 -> 0.5.1

--- pkgs/python/envier/default.nix
+++ pkgs/python/envier/default.nix
@@ -3,17 +3,20 @@
 , setuptools-scm
 , lib
 , python3
+, hatchling
+, hatch-vcs
 }:
 
 buildPythonPackage rec {
   pname = "envier";
-  version = "0.4.0";
+  version = "0.5.1";
+  format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "DataDog";
     repo = "envier";
     rev = "v${version}";
-    hash = "sha256-tQ4lWHsGZi1tnXTlvBCNzP3X3L3BwYPcCBp1hreMxeY=";
+    hash = "sha256-yWA9rNiKAkyQXFRFa/ggyih3aiqWsa37042Z8NoaGb0=";
   };
 
   SETUPTOOLS_SCM_PRETEND_VERSION = version;
@@ -22,7 +25,15 @@ buildPythonPackage rec {
     setuptools-scm
   ];
 
+  propagatedBuildInputs = [
+    hatchling
+    hatch-vcs
+  ];
+
   doCheck = false; # Fix-Me: Needs work
+
+  # Check for dependencies at:
+  # https://github.com/DataDog/envier/blob/main/pyproject.toml
 
   # nativeCheckInputs = with python3.pkgs; [
   #   pytestCheckHook
