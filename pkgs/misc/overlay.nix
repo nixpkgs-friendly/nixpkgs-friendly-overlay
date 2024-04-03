6b4c2cddf651418993fa66b34cda6f16b5d0d72e
superherointj <5861043+superherointj@users.noreply.github.com>
2024-04-03 15:37:47 -0300

    yandex-browser: remove

--- pkgs/misc/overlay.nix
+++ pkgs/misc/overlay.nix
@@ -31,11 +31,4 @@ rec {
 
   vimPlugins = import ./vim-plugins/default.nix final prev;
 
-  #yandex-browser-stable = callPackage ./yandex-browser { };
-  #yandex-browser-beta = yandex-browser.override({
-  #  channel = "beta";
-  #  version = "23.5.1.754-1";
-  #  hash = "sha256-ngtwrq8vDEt39Zd5jpBadouN1V8ly03la69M0AUyhGM=";
-  #});
-  #yandex-browser = yandex-browser-stable;
 }
