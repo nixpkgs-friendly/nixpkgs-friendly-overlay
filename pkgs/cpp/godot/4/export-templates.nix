1ad9e3937328003f169226efd2b313f47c0bf69f
superherointj <5861043+superherointj@users.noreply.github.com>
2024-04-18 16:44:16 -0300

    godot_4: remove

--- pkgs/cpp/godot/4/export-templates.nix
+++
@@ -1,8 +0,0 @@
-{ fetchzip, ... }:
-
-fetchzip {
-  pname = "export_templates";
-  extension = "zip";
-  url = "https://github.com/godotengine/godot/releases/download/4.2.1-stable/Godot_v4.2.1-stable_export_templates.tpz";
-  hash = "sha256-Ta+I5pmWyQGu5rH1yLMunF82uRLYcPobsreg6fgubxc=";
-}
--- pkgs/cpp/overlay.nix
+++ pkgs/cpp/overlay.nix
@@ -4,35 +4,6 @@ let
   callPackage = final.callPackage;
   pickLatest = (import ../../utils.nix).pickLatest; # Fix-Me
 in
-rec {
-  godot_4_2_2-rc2 =
-    let
-      commit = "c61a68614e5b030a4a1e11abaa5a893b8017f78d";
-    in
-    prev.godot_4.overrideAttrs (oa: rec {
-      GODOT_VERSION_STATUS = "rc2";
-      version = "4.2.2-${GODOT_VERSION_STATUS}";
-      src = final.fetchFromGitHub {
-        owner = "godotengine";
-        repo = "godot";
-        rev = commit;
-        hash = "sha256-bqEy5bB9YpeYgRxO81LUj/UG/uyNEcii9VECzfZeppo=";
-      };
-      preConfigure = ''
-        mkdir -p .git
-        echo ${commit} > .git/HEAD
-      '';
-    });
-  godot_4 = pickLatest (godot_4_2_2-rc2) prev.godot_4;
+{
 
-  godot_4_2_2-rc2-export-templates = prev.fetchzip {
-    pname = "export_templates";
-    extension = "zip";
-    url = "https://github.com/godotengine/godot-builds/releases/download/4.2.2-rc2/Godot_v4.2.2-rc2_export_templates.tpz";
-    hash = "sha256-Cb5s2u3zCEKqKqBDbt5T8ONXPosuDsrT1SCtaZX/Zfk=";
-  };
-  godot_4-export-templates = pickLatest (godot_4_2_2-rc2-export-templates) (
-    prev.godot_4-export-templates or null
-  );
-  # godot_4-export-templates = callPackage ./godot/4/export-templates.nix { };
 }
