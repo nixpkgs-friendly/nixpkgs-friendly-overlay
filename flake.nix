71b683216bd86a7a3b6980ac29e3439594a04ba8
superherointj <5861043+superherointj@users.noreply.github.com>
2024-04-19 15:10:33 -0300

    default to using nixpkgs/nixos-unstable

--- flake.nix
+++ flake.nix
@@ -1,7 +1,8 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixpkgs-friendly/nixpkgs-friendly/friendly";
-    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # Whenever friendly repository doesn't have patches, we default to using nixpkgs/nixos-unstable.
+    #nixpkgs.url = "github:nixpkgs-friendly/nixpkgs-friendly/friendly";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
   };
 
