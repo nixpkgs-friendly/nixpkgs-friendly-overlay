9874d7912dbd8c311894ff028ba66cdc2c910160
superherointj <5861043+superherointj@users.noreply.github.com>
2024-05-28 10:54:03 -0300

    bicep-lsp: fix 1

--- pkgs/dotnet/bicep-lsp/default.nix
+++ pkgs/dotnet/bicep-lsp/default.nix
@@ -6,6 +6,10 @@
   fetchFromGitHub,
   dotnetCorePackages,
   mono,
+  patchelf,
+  icu,
+  openssl,
+  coreutils
 }:
 
 buildDotnetModule rec {
@@ -27,7 +31,7 @@ buildDotnetModule rec {
 
   dotnet-runtime = dotnetCorePackages.runtime_8_0;
 
-  nativeBuildInputs = lib.optionals stdenv.isLinux [ autoPatchelfHook ];
+  nativeBuildInputs = lib.optionals stdenv.isLinux [ patchelf ]; # autoPatchelfHook 
 
   #doCheck = !(stdenv.isDarwin && stdenv.isAarch64); # mono is not available on aarch64-darwin
   # 1 test fails: Verify_snippet_cache
@@ -45,6 +49,67 @@ buildDotnetModule rec {
   #     --add-needed libc.so \
   #     $out/lib/bicep-lsp/libSystem.IO.Ports.Native.so
   # '';
+
+
+
+  postPatch =
+    ''
+      patchelf_add_icu_as_needed() {
+        declare elf="''${1?}"
+        declare icu_major_v="${lib.head (lib.splitVersion (lib.getVersion icu.name))}"
+
+        for icu_lib in icui18n icuuc icudata; do
+          patchelf --add-needed "lib''${icu_lib}.so.$icu_major_v" "$elf"
+        done
+      }
+
+      patchelf_common() {
+        declare elf="''${1?}"
+
+        patchelf_add_icu_as_needed "$elf"
+        patchelf --add-needed "libssl.so" "$elf"
+        patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+          --set-rpath "${
+            lib.makeLibraryPath [
+              stdenv.cc.cc
+              openssl
+              icu.out
+            ]
+          }:\$ORIGIN" \
+          "$elf"
+      }
+
+      # substituteInPlace dist/extension.js \
+      #   --replace 'uname -m' '${lib.getExe' coreutils "uname"} -m'
+
+    '';
+    # + (lib.concatStringsSep "\n" (
+    #   map (bin: ''
+    #     chmod +x "${bin}"
+    #   '') extInfo.binaries
+    # ))
+    # + lib.optionalString stdenv.isLinux (
+    #   lib.concatStringsSep "\n" (
+    #     map (bin: ''
+    #       patchelf_common "${bin}"
+    #     '') extInfo.binaries
+    #   )
+    #);
+
+
+
+#  >     libgcc_s.so.1 -> not found!
+#        > auto-patchelf: 6 dependencies could not be satisfied
+#        > error: auto-patchelf could not satisfy dependency libssl.so wanted by /nix/store/28b8n3wlmsn7mpzgzr2qhx1nb9i6gzzv-bicep-lsp-0.27.1/lib/bicep-lsp/Bicep.LangServer
+#        > error: auto-patchelf could not satisfy dependency libgssapi_krb5.so wanted by /nix/store/28b8n3wlmsn7mpzgzr2qhx1nb9i6gzzv-bicep-lsp-0.27.1/lib/bicep-lsp/Bicep.LangServer
+#        > error: auto-patchelf could not satisfy dependency libicui18n.so wanted by /nix/store/28b8n3wlmsn7mpzgzr2qhx1nb9i6gzzv-bicep-lsp-0.27.1/lib/bicep-lsp/Bicep.LangServer
+#        > error: auto-patchelf could not satisfy dependency libicuuc.so wanted by /nix/store/28b8n3wlmsn7mpzgzr2qhx1nb9i6gzzv-bicep-lsp-0.27.1/lib/bicep-lsp/Bicep.LangServer
+#        > error: auto-patchelf could not satisfy dependency libstdc++.so.6 wanted by /nix/store/28b8n3wlmsn7mpzgzr2qhx1nb9i6gzzv-bicep-lsp-0.27.1/lib/bicep-lsp/Bicep.LangServer
+#        > error: auto-patchelf could not satisfy dependency libgcc_s.so.1 wanted by /nix/store/28b8n3wlmsn7mpzgzr2qhx1nb9i6gzzv-bicep-lsp-0.27.1/lib/bicep-lsp/Bicep.LangServer
+#        > auto-patchelf failed to find all the required dependencies.
+#        > Add the missing dependencies to --libs or use `--ignore-missing="foo.so.1 bar.so etc.so"`.
+
+
 
   #libSystem.IO.Ports.Native.so
   #	linux-vdso.so.1 (0x00007ffe06aad000)
