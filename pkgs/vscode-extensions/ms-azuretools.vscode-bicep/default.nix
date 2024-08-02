ef111555d46430919d42973477931313d7cf0347
superherointj <5861043+superherointj@users.noreply.github.com>
2024-08-02 16:05:31 -0300

    vscode-extensions.ms-azuretools.vscode-bicep: extra information

--- pkgs/vscode-extensions/ms-azuretools.vscode-bicep/default.nix
+++ pkgs/vscode-extensions/ms-azuretools.vscode-bicep/default.nix
@@ -30,8 +30,16 @@ vscode-utils.buildVscodeMarketplaceExtension {
     bicep-lsp
   ];
 
-  meta.downloadPage = "https://marketplace.visualstudio.com/items?itemName=ms-azuretools.vscode-bicep";
+  meta = {
+    downloadPage = "https://marketplace.visualstudio.com/items?itemName=ms-azuretools.vscode-bicep";
+    homepage = "https://github.com/Azure/bicep/tree/main/src/vscode-bicep";
+  };
 }
+
+# From: https://github.com/Azure/bicep/blob/main/src/vscode-bicep/package.json#L818
+# Extension dependencies:
+#   - ms-dotnettools.vscode-dotnet-runtime
+#     https://github.com/dotnet/vscode-dotnet-runtime/blob/main/vscode-dotnet-runtime-extension/package.json#L83C6-L83C57
 
 # https://rakhesh.com/azure/bicep-stuck-on-registering-commands-in-vs-code/
 # Bicep language server is missing
