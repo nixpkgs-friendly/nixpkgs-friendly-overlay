ee9d0183509cde05570af128138ac597b125d1ba
superherointj <5861043+superherointj@users.noreply.github.com>
2025-04-15 09:58:53 -0300

    vscode-extensions: use pickLatest

--- pkgs/vscode-extensions/overlay.nix
+++ pkgs/vscode-extensions/overlay.nix
@@ -5,67 +5,66 @@ let
   pickLatest = (import ../../utils.nix).pickLatest; # Fix-Me
   extensionFromVscodeMarketplace = final.vscode-utils.extensionFromVscodeMarketplace;
 in
-# To-Do: Use "pickLatest" method for each extension here.
+
 {
   vscode-extensions = prev.vscode-extensions // {
 
-    andrew-butson.vscode-openai = extensionFromVscodeMarketplace {
+    andrew-butson.vscode-openai = pickLatest (extensionFromVscodeMarketplace {
       # https://marketplace.visualstudio.com/items?itemName=AndrewButson.vscode-openai
       publisher = "AndrewButson";
       name = "vscode-openai";
       version = "1.6.11";
       hash = "sha256-Yfsa33a1jvbgOBRK0aqcL6/IYc/jhcHmu8RHhA35J9A=";
-    };
+    }) (prev.vscode-extensions.andrew-butson.vscode-openai or null);
 
-    designliquido.designliquido-vscode = extensionFromVscodeMarketplace {
+    designliquido.designliquido-vscode = pickLatest (extensionFromVscodeMarketplace {
       # https://marketplace.visualstudio.com/items?itemName=designliquido.designliquido-vscode
       publisher = "designliquido";
       name = "designliquido-vscode";
       version = "0.12.24";
       hash = "sha256-MiVRVsyhjjxnyUeqC3sWpL4QGbSyoM53gWoK4C+qdK4=";
-    };
+    }) (prev.vscode-extensions.designliquido.designliquido-vscode or null);
 
-    gbtb.ponylang = extensionFromVscodeMarketplace {
+    gbtb.ponylang = pickLatest (extensionFromVscodeMarketplace {
       # https://marketplace.visualstudio.com/items?itemName=gbtb.ponylang
       publisher = "gbtb";
       name = "ponylang";
       version = "0.2.2";
       hash = "sha256-ypSEb4j9Gig0Cpo4cM5IoGMmKv1f9MLTfJCXBZAzg3M=";
-    };
+    }) (prev.vscode-extensions.gbtb.ponylang or null);
 
-    kcl.kcl-vscode-extension = extensionFromVscodeMarketplace {
+    kcl.kcl-vscode-extension = pickLatest (extensionFromVscodeMarketplace {
       # https://marketplace.visualstudio.com/items?itemName=kcl.kcl-vscode-extension
       publisher = "kcl";
       name = "kcl-vscode-extension";
       version = "0.2.2";
       hash = "sha256-iMRIe9IoVebu4KwIYw/558FPBL/NdDNDUy1le1luif8=";
       # Requires: kcl kcl-language-server
-    };
+    }) (prev.vscode-extensions.kcl.kcl-vscode-extension or null);
 
-    npruehs.pony = extensionFromVscodeMarketplace {
+    npruehs.pony = pickLatest (extensionFromVscodeMarketplace {
       # https://marketplace.visualstudio.com/items?itemName=npruehs.pony
       publisher = "npruehs";
       name = "pony";
       version = "0.0.4";
       hash = "sha256-iAs5YnaO8+GdPrcXupRVy80O45P23F2yRdXP2Dj7F3Y=";
-    };
+    }) (prev.vscode-extensions.npruehs.pony or null);
 
-    philhindle.errorlens = extensionFromVscodeMarketplace {
+    philhindle.errorlens = pickLatest (extensionFromVscodeMarketplace {
       # https://marketplace.visualstudio.com/items?itemName=PhilHindle.errorlens
       publisher = "PhilHindle";
       name = "errorlens";
       version = "1.1.3";
       hash = "sha256-kMbet43DvTGIui7fcP8amCVzS0C/KY9VBVlZ+MlTxyM=";
-    };
+    }) (prev.vscode-extensions.philhindle.errorlens or null);
 
-    saoudrizwan.claude-dev = extensionFromVscodeMarketplace {
+    # Test pick-latest here.
+    saoudrizwan.claude-dev = pickLatest (extensionFromVscodeMarketplace { # ALREADY UPSTREAMED REMOVE!!!
       # https://marketplace.visualstudio.com/items?itemName=saoudrizwan.claude-dev
       publisher = "saoudrizwan";
       name = "claude-dev";
       version = "3.2.5";
       hash = "sha256-aJnN5zjF6tvUSMqVklNgCgpsfBNi1vw0i66BBFgHB1o=";
-    };
+    }) (prev.vscode-extensions.saoudrizwan.claude-dev or null);
   };
 }
-
-# To-Do: x = pickLatest (callPackage ./x { }) (prev.x or null);
