13a32397b2dd68b3ec522f7dd12aa59edb22d3f0
superherointj <5861043+superherointj@users.noreply.github.com>
2025-09-17 14:32:35 -0300

    vscode-extensions: re-establish alphabetical order

--- pkgs/vscode-extensions/overlay.nix
+++ pkgs/vscode-extensions/overlay.nix
@@ -9,6 +9,14 @@ in
 {
   vscode-extensions = prev.lib.recursiveUpdate prev.vscode-extensions {
 
+    "498-00".cppformat = pickLatest (extensionFromVscodeMarketplace {
+      # https://marketplace.visualstudio.com/items?itemName=498-00.cppformat
+      publisher = "498-00";
+      name = "cppformat";
+      version = "1.0.10";
+      hash = "sha256-4mcWjmhw8+yB3anbWC2Lhc/QjUUHEOiMeghkqinyYHQ=";
+    }) (prev.vscode-extensions."498-00".cppformat or null);
+
     andrew-butson.vscode-openai = pickLatest (extensionFromVscodeMarketplace {
       # https://marketplace.visualstudio.com/items?itemName=AndrewButson.vscode-openai
       publisher = "AndrewButson";
@@ -74,6 +82,14 @@ in
       hash = "sha256-iAs5YnaO8+GdPrcXupRVy80O45P23F2yRdXP2Dj7F3Y=";
     }) (prev.vscode-extensions.npruehs.pony or null);
 
+    pflannery.vscode-versionlens = pickLatest (extensionFromVscodeMarketplace {
+      # https://marketplace.visualstudio.com/items?itemName=pflannery.vscode-versionlens
+      publisher = "pflannery";
+      name = "vscode-versionlens";
+      version = "1.22.3";
+      hash = "sha256-+975OorwHN2/DJpjO6ECp+tRBAyDjEFkpaT3eb7ibBM=";
+    }) (prev.vscode-extensions.pflannery.vscode-versionlens or null);
+
     philhindle.errorlens = pickLatest (extensionFromVscodeMarketplace {
       # https://marketplace.visualstudio.com/items?itemName=PhilHindle.errorlens
       publisher = "PhilHindle";
@@ -89,22 +105,6 @@ in
       version = "0.0.27";
       hash = "sha256-kl6W1UQ36cNQNj3cOsMyZbxD6glaRm3W0Z1W+xuEcjs=";
     }) (prev.vscode-extensions.vytautassurvila.csharp-ls or null);
-
-    pflannery.vscode-versionlens = pickLatest (extensionFromVscodeMarketplace {
-      # https://marketplace.visualstudio.com/items?itemName=pflannery.vscode-versionlens
-      publisher = "pflannery";
-      name = "vscode-versionlens";
-      version = "1.22.3";
-      hash = "sha256-+975OorwHN2/DJpjO6ECp+tRBAyDjEFkpaT3eb7ibBM=";
-    }) (prev.vscode-extensions.pflannery.vscode-versionlens or null);
-
-    "498-00".cppformat = pickLatest (extensionFromVscodeMarketplace {
-      # https://marketplace.visualstudio.com/items?itemName=498-00.cppformat
-      publisher = "498-00";
-      name = "cppformat";
-      version = "1.0.10";
-      hash = "sha256-4mcWjmhw8+yB3anbWC2Lhc/QjUUHEOiMeghkqinyYHQ=";
-    }) (prev.vscode-extensions."498-00".cppformat or null);
 
   };
 }
